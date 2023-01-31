1e2f0b76e1dcd1ac51f42f90735f174aa88582cd
Khaled Hammouda <khaledh@gmail.com>
2023-01-31 08:16:27 -0500

    update

--- timeline/admin.py
+++ timeline/admin.py
@@ -1,13 +1,34 @@
+from datetime import date
+
 from django.contrib import admin
 
 from timeline.models import Entry, Topic
 
 
+class DecadeFilter(admin.SimpleListFilter):
+    title = 'decade'
+    parameter_name = 'decade'
+
+    def lookups(self, request, model_admin):
+        return [
+            (year, f'{year}s')
+            for year in range(2020, 1800 - 1, -10)
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            decade = int(self.value())
+            return queryset.filter(
+                date__gte=date(decade, 1, 1),
+                date__lte=date(decade + 9, 12, 31),
+            )
+
+
 @admin.register(Entry)
 class EntryAdmin(admin.ModelAdmin):
     list_display = ('title', 'topics_list', 'date')
+    list_filter = [DecadeFilter, 'topics']
     autocomplete_fields = ['topics']
-    # filter_horizontal = ['topics']
     search_fields = ['title']
 
     def topics_list(self, entry):
